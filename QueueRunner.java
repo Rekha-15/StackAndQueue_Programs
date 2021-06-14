29b1a30cf15d3489b6441a18019cebcadc914acc
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-14 04:52:40 -0400

    add : QueueRunner.java

---
+++ QueueRunner.java
@@ -0,0 +1,14 @@
+package com;
+
+public class QueueRunner {
+	
+	public static void main(String[] args) {
+		QueueList<Integer> queue = new QueueList<Integer>();
+		queue.enqueue(56);
+		queue.enqueue(30);
+		queue.enqueue(70);
+		queue.show();
+		queue.dequeue();
+	}
+}
+
