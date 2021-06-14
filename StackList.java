c569a6a54e069ef0d736c8a2f908accad68c6e85
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-14 03:23:42 -0400

    add : StackList.java

--- StackList.java
+++ StackList.java
@@ -4,14 +4,44 @@ package com;
  * @author rekha
  * StackList is a class of public type
  * @param <T>
- * printing welcome statement
  */
 
 public class StackList <T> {
 	
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Stack and Queue Program");
-		
+int size;
+	
+  Node<T> top;   
+  
+  /**
+   * @author rekha
+   *push method used to inserting the elements into stack
+   * @param <T>
+   * called new Data
+   */
+	
+	public void push(T newData) {
+		Node<T> newNode = new Node<>();
+		newNode.data = newData;
+		newNode.next = null;
+		newNode.next = top;
+		top = newNode;
 	}
+	
+	/**
+	 * Show method uses to display the elements
+	 *using while condition to check next node is null or not
+	 *loop will be active still it reaches the null
+	 * @param <T>
+	 */
+	
+	public void show() {
+		
+		Node<T> currNode = top;
+		while (currNode.next != null) {
+			System.out.println(currNode.data);
+			currNode = currNode.next;
+		}
+		System.out.println(currNode.data);
+	}	
+	
 }
