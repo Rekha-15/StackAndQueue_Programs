4d00a949f8565803ddcd4d73db5125b15956780e
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-14 03:19:03 -0400

    add : StackList.java

---
+++ StackList.java
@@ -0,0 +1,17 @@
+package com;
+
+/**
+ * @author rekha
+ * StackList is a class of public type
+ * @param <T>
+ * printing welcome statement
+ */
+
+public class StackList <T> {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Stack and Queue Program");
+		
+	}
+}
